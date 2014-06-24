934ab60206546264bde48ab2ecba05217eac6b29
Waqas Younas <waqas.younas@gmail.com>
2014-06-24 18:15:48 +0500

    Adding Base class for commands

---
+++ lib/commands/base.py
@@ -0,0 +1,39 @@
+from ConfigParser import SafeConfigParser
+import argparse
+
+class BaseCommand(object):
+
+    def __init__(self, *args, **kwargs):
+        self.cfg_parser = SafeConfigParser()
+        self.cfg_parser.read("../app_config.cfg")
+        self.username = self.cfg_parser.get('app_level', 'username')
+        self.password = self.cfg_parser.get('app_level', 'password') 
+        self.app_base_url = self.cfg_parser.get('app_level', 'app_base_url')
+        self.args = None
+    def create_parser(self):
+        
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument('--username', help="Username used to login", \
+                default=self.username)
+        self.parser.add_argument('--password', help='Password used to login',\
+                default=self.password)
+        self.parser.add_argument('--app_base_url', help="Application's base URL", \
+                default=self.app_base_url)
+
+
+        self.add_arguments()
+        self.args = self.parser.parse_args()
+
+    def add_arguments(self):
+        """
+        Entrypoint for subclasses to add arguments
+        """
+        pass
+
+    def excute(self):
+        pass
+
+
+b = BaseCommand()
+b.create_parser()
+print b.args
