83954644c02dcf0fad86c6aaa1c8e68f9b176265
Waqas Younas <waqas.younas@gmail.com>
2014-07-20 13:54:04 +0500

    Fixing the config path bug

--- lib/commands/base.py
+++ lib/commands/base.py
@@ -11,7 +11,7 @@ class BaseCommand(object):
         self.cfg_parser = SafeConfigParser()
         os.path.abspath(CURRENT_DIR)
         config_path = os.chdir('..') + '/app_config.cfg'
-        print 'Config path ' + config_path
+        print('Config path ' + config_path)
         self.cfg_parser.read(config_path)
         self.username = self.cfg_parser.get('app_level', 'username')
         self.password = self.cfg_parser.get('app_level', 'password') 
