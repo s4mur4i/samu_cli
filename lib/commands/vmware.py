37ab4bbd35d7e4604c26fbd2260727d274be9371
Krisztian Banhidy <s4mur4i@balabit.hu>
2014-12-11 14:00:37 +0100

    Vmware: added folder tasks

--- lib/commands/vmware.py
+++ lib/commands/vmware.py
@@ -127,7 +127,68 @@ Example:
     print "implementing"
 
   def folder(self):
-    print "implementing"
+    self.check_session_exists()
+    parser = argparse.ArgumentParser( description='Samu tool for Support',  usage= '''samu.py vmware folder [<args>]]
+
+Folder endpoint profile
+  --moref <moref>
+  --move_child_moref <moref>
+  --move_child_type <VMware object type>
+  --move_parent <moref>
+  --create_name <folder_name>
+  --delete
+
+Example:
+  samu.py vmware folder 
+  samu.py vmware folder --moref group-v111
+
+  samu.py vmware folder --create_name herring
+  samu.py vmware folder --moref group-v111 --create_name herring
+
+  samu.py vmware folder --moref group-v111 --delete
+
+  samu.py vmware folder --move_child_moref group-v112 --move_child_type Folder 
+  samu.py vmware folder --move_child_moref group-v112 --move_child_type Folder --move_parent group-v111
+  samu.py vmware folder --move_child_moref group-v112 --move_child_type Folder --moref group-v111
+    ''')
+    parser.add_argument('--moref',  default=None,  help="Moref of folder")
+    parser.add_argument('--delete',  action='store_true',  help="Delete the folder")
+    parser.add_argument('--create_name',  default=None,  help="Name of folder to create")
+    parser.add_argument('--move_child_moref',  default=None,  help="Child moref to move")
+    parser.add_argument('--move_child_type',  default=None,  help="Type of child moref to move")
+    parser.add_argument('--move_parent',  default=None,  help="Parent moref to move to")
+    args = parser.parse_args(sys.argv[3:])
+    resp = None
+    if args.moref is not None:
+      url = self.vmware_url + "/folder/" + args.moref + "/-/" + self.sessionid
+      if args.create_name is not None:
+        self.payload['name'] = args.create_name
+        resp = requests.post(url,  data=self.http_payload(self.payload)).json()
+      elif args.move_child_moref is not None:
+        self.payload['child_value'] = args.move_child_moref
+        self.payload['child_type'] = args.move_child_type
+        resp = requests.put(url,  data=self.http_payload(self.payload)).json()
+      elif args.delete == True:
+        resp = requests.delete(url,  data=self.http_payload(self.payload)).json()
+      else:
+        resp = requests.get(url, data=self.http_payload(self.payload)).json()
+    else:
+      url = self.vmware_url + "/folder/-/" + self.sessionid
+      if args.create_name is not None:
+        self.payload['name'] = args.create_name
+        resp = requests.post(url,  data=self.http_payload(self.payload)).json()
+      elif args.move_child_moref is not None:
+        self.payload['child_value'] = args.move_child_moref
+        self.payload['child_type'] = args.move_child_type
+        if args.move_parent is not None:
+          self.payload['parent_value'] = args.move_parent
+        resp = requests.put(url,  data=self.http_payload(self.payload)).json()
+      else:
+        resp = requests.get(url, data=self.http_payload(self.payload)).json()
+    self.logger.debug("Response is: %s" % resp)
+    self.check_status(resp)
+    self.output(resp['result'])
+
 
   def user(self):
     self.check_session_exists()
